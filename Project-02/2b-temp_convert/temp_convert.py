c45770263da02439e5d086a49b0d56b94bb30432
Timur Guner <76972389+timurguner@users.noreply.github.com>
2022-05-13 00:05:09 -0700

    Add files via upload

---
+++ Project-02/2b-temp_convert/temp_convert.py
@@ -0,0 +1,12 @@
+# Author: Timur
+# Date: 2021-01-13
+# Description: This converts Celsius to Fahrenheit
+
+# ask user to enter a celsius and save it to celsius
+print("Please enter a Celsius temperature.")
+celsius = float(input())
+
+# convert to fahrenheit and print
+fahrenheit = celsius * (9 / 5) + 32
+print("The equivalent Fahrenheit temperature is:")
+print(fahrenheit)
